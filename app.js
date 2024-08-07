341430c5519efb2a2ec00ab95c6518d6a55307d8
Prem <premmittal88@gmail.com>
2024-08-07 17:03:26 +0530

    dfd

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(cors());
 const allowedOrigins = ["http://localhost:5173"];
 app.use(
@@ -13,7 +15,7 @@ app.use(
 );
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+
 app.use(cookieParser());
 
 import userRouter from "./routes/user.route.js";
@@ -21,5 +23,10 @@ app.use("/api/v1/users", userRouter);
 
 import slotRouter from "./routes/slot.route.js";
 app.use("/api/v1/slots", slotRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+);
 
 export default app;
